0660276f0cddc6ac9396670ad1908a3a0dba6dd8
kitamuraharuka <kitaharu65@gmail.com>
2020-08-03 11:13:37 +0000

    ８/２メンタリングで訂正した箇所

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,20 +1,6 @@
 @extends('layouts.profile')
-@section('title', 'ニュースの新規作成')
+@section('title', 'ニュース新規作成')
 @section('content')
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"
-        content="IE=edge">
-        <meta name="viewport"
-        content="width=device-width, initial-scale=1">
-        
-        <title>プロフィール
-    </title>
-    </head>
-    <body>
-        <h1>マイプロフィール</h1>
-    </body>
-</html>
-
+    <h1>マイプロフィール</h1>
+    <p>私の名前ははるかです</p>
 @endsection
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,21 +1,6 @@
 @extends('layouts.profile')
-@section('title', 'プロフィールの新規作成')
-
-
+@section('title', 'プロフィール編集')
 @section('content')
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible"
-        content="IE=edge">
-        <meta name="viewport"
-        content="width=device-width, initial-scale=1">
-        
-        <title>unun</title>
-    </head>
-    <body>
-        <h1>un</h1>
-    </body>
-</html>
+    <h1>編集画面</h1>
 
 @endsection
