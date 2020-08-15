7b5a28bec6ffc2a861cf2000dae2b545103241a3
kitamuraharuka <kitaharu65@gmail.com>
2020-08-15 11:35:29 +0000

    lesson116 更新

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -4,7 +4,7 @@
   <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース編集</h2>
+                <h2>プロフィール編集</h2>
                 <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
@@ -42,7 +42,7 @@
                         <div class="col-md-10">
                             <input type="file" class="form-control-file" name="image">
                             <div class="form-text text-info">
-                                設定中: {{ $news_form->image_path }}
+                                設定中: {{ $profile_form->image_path }}
                             </div>
                             <div class="form-check">
                                 <label class="form-check-label">
